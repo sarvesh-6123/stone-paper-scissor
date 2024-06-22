4d38f60cc6a3a4eca9cc7f62e1c746732d01dde6
Sarvesh Varaa <sarveshvaraams6123@gmail.com>
2024-06-22 21:51:31 +0530

    second

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function winorloose(){
-    let 
+function winorloose() {
+  let choice = document.getElementById("choice").value;
+  let button = document.getElementById("submit-button");
+  let result = document.getElementById("result");
+  let machine = document.getElementById("mychoice");
+  let resText = "Sorry You Loose..!";
+  let choicearr = ["stone", "paper", "sicssor"];
+  console.log(choice == "empty");
+  if (choice !== "empty") {
+    let x = parseInt(Math.random() * 10);
+    let myChoice = choicearr[x % 3];
+    machine.innerHTML = "My Choice is " + myChoice.toLocaleUpperCase();
+    console.log(myChoice);
+    if (myChoice === choice) {
+      resText = "Match is Draw..!";
+    } else if (
+      (choice === "stone" && myChoice === "sicssor") ||
+      (choice === "paper" && myChoice === "stone") ||
+      (choice === "sicssor" && myChoice === "paper")
+    ) {
+      resText = "You Won The Match.. :)";
+    }
+  } else {
+    resText = "Please Enter a Choice :(";
+  }
+  button.style.display = "None";
+  result.innerHTML = resText;
 }
